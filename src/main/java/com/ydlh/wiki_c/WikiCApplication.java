cc6353b4dd5442d7308813dc3dd9de3ef004e5a6
chen63wuya <chen63wuya@qq.com>
2021-06-16 01:19:43 +0800

    这是第一个版本

---
+++ src/main/java/com/ydlh/wiki_c/WikiCApplication.java
@@ -0,0 +1,13 @@
+package com.ydlh.wiki_c;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikiCApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WikiCApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ydlh/wiki_c/WikiCApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ydlh.wiki_c;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiCApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
